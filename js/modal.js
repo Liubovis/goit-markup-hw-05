45f68cba9099ec67f27673cf80f089d8d9b71906
Liubov Kochmaruk <l.s.kochmaruk@gmail.com>
2022-10-01 15:12:53 +0200

    finished modal, added modal.js script

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,24 @@
 (() => {
-  const refs = {
+  const {
+    openModalBtn,
+    closeModalBtn,
+    modal,
+    body
+  } = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    body: document.querySelector("body")
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  openModalBtn.addEventListener("click", toggleModal);
+  closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    modal.classList.toggle("is-hidden");
+
+    !modal.classList.contains("is-hidden")
+      ? body.style.overflow = "hidden"
+      : body.style.overflow = "auto";
   }
 })();
